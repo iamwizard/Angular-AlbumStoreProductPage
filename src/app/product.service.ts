546f17e8880cf149715aed486d624626c8a97050
Amardeep Singh Suri <amardeepsinghsuri@gmail.com>
2020-07-26 18:07:48 +0200

    using interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,6 +14,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) : Observable<Album> {
-    return this._http.get<Album>(this._albumUrl).map((response) => response.json<Album>());
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 }
