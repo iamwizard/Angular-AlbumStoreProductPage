895d094de6c20ce10de71be177891b6bae35a022
Amardeep Singh Suri <amardeepsinghsuri@gmail.com>
2020-07-26 17:19:02 +0200

    part3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,5 +12,5 @@ export class ProductService {
 
   getAlbum(id: number) {
    return this._http.get(this._albumUrl).map((response) => response.json()); //Mapping The JSON Response in getAlbum
-}
+  }
 }
